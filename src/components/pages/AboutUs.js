e5783d5eeca51c98d300e6d93c05f24473b78fc1
Dhawal Kumar Singh <dhawal.singh10@gmail.com>
2021-04-08 04:28:03 +0530

    Basic Functionality Ready

---
+++ src/components/pages/AboutUs.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const AboutUs = () => {
+  return (
+    <>
+      <h1>About This App</h1>
+      <p>Application To Search Github Users</p>
+      <p>Version 1.0.0</p>
+    </>
+  );
+};
